fdb9135e50836ac895f0b8836955a809fa952ed3
sungjin <byun970@gmail.com>
2024-08-14 13:29:19 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://byun:qq00qq00@boilerplate.72ganxx.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useCreateIndex: true,
-      // useFindAndModifiy: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModifiy: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! 안녕하세요~~");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const userInfo = await user.save();
+    res.status(200).json({ success: true, userInfo });
+  } catch (err) {
+    res.json({ sucess: false, err });
+  }
 });
 
 app.listen(port, () => {
